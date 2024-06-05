3128fb037e324807a0216a7e83d0d5c795a8f2fa
alvihelene <95623140+alvihelene@users.noreply.github.com>
2024-06-05 15:04:52 +0200

    linting

--- eslint.config.js
+++ eslint.config.js
@@ -4,12 +4,28 @@ import pluginVue from 'eslint-plugin-vue'
 
 export default [
   {
-    languageOptions: { globals: globals.browser },
+    env: {
+      node: true // Add this line
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        require: 'readonly', // Add this line
+        module: 'readonly' // Add this line
+      }
+    },
     files: ['src/**/*.{js,mjs,vue}'],
     ignores: ['node_modules', 'dist', 'build', 'public', '.gitignore'],
     rules: {
       semi: 'error'
     }
+  },
+  {
+    files: ['cypress/**/*.{js,ts,jsx,tsx}'],
+    env: {
+      'cypress/globals': true // Add this line
+    },
+    extends: ['plugin:cypress/recommended']
   },
   pluginJs.configs.recommended,
   ...pluginVue.configs['flat/essential']
