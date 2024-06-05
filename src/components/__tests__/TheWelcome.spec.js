88bae03a56ba4d1a3d40716d16391246f1155055
alvihelene <95623140+alvihelene@users.noreply.github.com>
2024-06-05 14:00:25 +0200

    testing

--- src/components/__tests__/TheWelcome.spec.js
+++ src/components/__tests__/TheWelcome.spec.js
@@ -1,20 +1,20 @@
-import { describe, it, expect } from 'vitest'
-import { mount } from '@vue/test-utils'
-import TheWelcome from '../TheWelcome.vue'
+import { describe, it, expect } from 'vitest';
+import { mount } from '@vue/test-utils';
+import TheWelcome from '../TheWelcome.vue';
 
 describe('TheWelcome.vue', () => {
   it('renders WelcomeItem component', () => {
-    const wrapper = mount(TheWelcome)
-    expect(wrapper.findComponent({ name: 'WelcomeItem' })).toBeTruthy()
-  })
+    const wrapper = mount(TheWelcome);
+    expect(wrapper.findComponent({ name: 'WelcomeItem' })).toBeTruthy();
+  });
 
   it('renders DocumentationIcon', () => {
-    const wrapper = mount(TheWelcome)
-    expect(wrapper.findComponent({ name: 'DocumentationIcon' })).toBeTruthy()
-  })
+    const wrapper = mount(TheWelcome);
+    expect(wrapper.findComponent({ name: 'DocumentationIcon' })).toBeTruthy();
+  });
 
   it('contains link to Vue documentation', () => {
-    const wrapper = mount(TheWelcome)
-    expect(wrapper.html()).toContain('https://vuejs.org/')
-  })
-})
+    const wrapper = mount(TheWelcome);
+    expect(wrapper.html()).toContain('https://vuejs.org/');
+  });
+});
